e26ed20686944c3ad648d492cb5267a0ff56634f
andreyxdd <andrei.v@ton.org>
2026-05-19 10:02:07 +0700

    remove pi-hermes-memory and pi-graphify extensions clean repo from hermes/graphify artifacts update .gitignore accordingly

--- extensions/pi-graphify/evals/run.js
+++
@@ -1,3 +0,0 @@
-const { createJiti } = require("/Users/andreyxdd/.nvm/versions/node/v24.14.0/lib/node_modules/@earendil-works/pi-coding-agent/node_modules/jiti");
-const jiti = createJiti(__filename);
-jiti("./graphify-extension.test.ts");
--- extensions/pi-graphify/index.ts
+++
@@ -1,6 +0,0 @@
-import graphifyExtension from "./graphify";
-import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
-
-export default function (pi: ExtensionAPI) {
-  graphifyExtension(pi);
-}
